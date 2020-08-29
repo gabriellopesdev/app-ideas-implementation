0239ea3337d6782828b4ce174854c4f2aba63249
gabriellopesdev <gabslopes34@gmail.com>
2020-08-28 23:19:47 -0300

    final implementation and add some style

--- border-radius-preview/src/components/borderPreview/index.tsx
+++ border-radius-preview/src/components/borderPreview/index.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
+import { FiCopy } from 'react-icons/fi';
 import './style.css'
 
 function BorderPreview() {
     const [horizontalTopLeft, setHorizontalTopLeft] = useState('10')    
-    const [horizontalTopRight, setHorizontalTopRight] = useState('30')
+    const [horizontalTopRight, setHorizontalTopRight] = useState('10')
     const [horizontalBottomRight, setHorizontalBottomRight] = useState('20')
     const [horizontalBottomLeft, setHorizontalBottomLeft] = useState('10')
 
@@ -12,6 +13,14 @@ function BorderPreview() {
     const [verticalBottomRight, setVerticalBottomRight] = useState('20')
     const [verticalBottomLeft, setVerticalBottomLeft] = useState('10')
 
+    function copyToClipboard(){
+        var textField = document.createElement('textarea')
+        textField.innerText = 'border-radius: ' + borderRadiusStyle.borderRadius + ';'
+        document.body.appendChild(textField)
+        textField.select()
+        document.execCommand('copy')
+        textField.remove()
+    }
     function handleValue(value: string, componentName: string) {
         if (Number(value) || !value)  {
             if (Number(value) > 100) {
@@ -48,95 +57,134 @@ function BorderPreview() {
     }
     const borderRadiusStyle = {
         borderRadius: 
-            horizontalTopLeft + '%' +
-            horizontalTopRight + '%' +   
-            horizontalBottomRight + '%' +
-            horizontalBottomLeft + '%' +
+            horizontalTopLeft + '% ' +
+            horizontalTopRight + '% ' +   
+            horizontalBottomRight + '% ' +
+            horizontalBottomLeft + '% ' +
              ' / ' +
-            verticalTopLeft + '%' +
-            verticalTopRight + '%' +
-            verticalBottomRight + '%' +
-            verticalBottomLeft + '%' 
+            verticalTopLeft + '% ' +
+            verticalTopRight + '% ' +
+            verticalBottomRight + '% ' +
+            verticalBottomLeft + '% ' 
     }
     return (
-        <div>
-            <input                 
-                type="text"                         
-                value={horizontalTopLeft}        
-                name="horizontalTopLeft"
-                onChange={(e) => {
-                    handleValue(e.target.value, e.target.name)             
-                }}
-            >
-            </input>
-            <input                 
-                type="text"                         
-                value={horizontalTopRight}        
-                name="horizontalTopRight"
-                onChange={(e) => {
-                    handleValue(e.target.value, e.target.name)             
-                }}
-            >
-            </input>
-            <input                 
-                type="text"                         
-                value={horizontalBottomRight}        
-                name="horizontalBottomRight"
-                onChange={(e) => {
-                    handleValue(e.target.value, e.target.name)             
-                }}
-            >
-            </input>
-            <input                 
-                type="text"                         
-                value={horizontalBottomLeft}        
-                name="horizontalBottomLeft"
-                onChange={(e) => {
-                    handleValue(e.target.value, e.target.name)             
-                }}
-            >
-            </input>
-            <input                 
-                type="text"                         
-                value={verticalTopLeft}        
-                name="verticalTopLeft"
-                onChange={(e) => {
-                    handleValue(e.target.value, e.target.name)             
-                }}
-            >
-            </input>
-            <input                 
-                type="text"                         
-                value={verticalTopRight}        
-                name="verticalTopRight"
-                onChange={(e) => {
-                    handleValue(e.target.value, e.target.name)             
-                }}
-            >
-            </input>
-            <input                 
-                type="text"                         
-                value={verticalBottomRight}        
-                name="verticalBottomRight"
-                onChange={(e) => {
-                    handleValue(e.target.value, e.target.name)             
-                }}
-            >
-            </input>
-            <input                 
-                type="text"                         
-                value={verticalBottomLeft}        
-                name="verticalBottomLeft"
-                onChange={(e) => {
-                    handleValue(e.target.value, e.target.name)             
-                }}
-            >
-            </input>
-
-            <div 
-                className="bordered-rec"
-                style={borderRadiusStyle}
-            >
+        <div className="content">            
+            <div className="line topLine">
+                <input       
+                    size={1}          
+                    type="text"                         
+                    value={horizontalTopLeft}        
+                    name="horizontalTopLeft"
+                    onChange={(e) => {
+                        handleValue(e.target.value, e.target.name)             
+                    }}
+                >
+                </input>
+                <input  
+                    size={1}                 
+                    type="text"                         
+                    value={horizontalTopRight}        
+                    name="horizontalTopRight"
+                    onChange={(e) => {
+                        handleValue(e.target.value, e.target.name)             
+                    }}
+                >
+                </input>
+            
+            </div> 
+            <br/>
+            <div className="line centerLine">
+                <div className="column">
+                    <input    
+                        size={1}               
+                        type="text"                         
+                        value={verticalTopLeft}        
+                        name="verticalTopLeft"
+                        onChange={(e) => {
+                            handleValue(e.target.value, e.target.name)             
+                        }}
+                    >
+                    </input>
+                    <input        
+                        size={1}           
+                        type="text"                         
+                        value={verticalBottomLeft}        
+                        name="verticalBottomLeft"
+                        onChange={(e) => {
+                            handleValue(e.target.value, e.target.name)             
+                        }}
+                    >
+                    </input>
+                </div>
+                <div className="container">
+                    <div 
+                        className="bordered-rec"
+                        style={borderRadiusStyle}
+                    >
+                    </div>
+                </div>               
+               
+                <div className="column">
+                    <input   
+                        size={1}                
+                        type="text"                         
+                        value={verticalTopRight}        
+                        name="verticalTopRight"
+                        onChange={(e) => {
+                            handleValue(e.target.value, e.target.name)             
+                        }}
+                    >
+                    </input> 
+                    
+                    <input     
+                        size={1}             
+                        type="text"                         
+                        value={verticalBottomRight}        
+                        name="verticalBottomRight"
+                        onChange={(e) => {
+                            handleValue(e.target.value, e.target.name)             
+                        }}
+                    >
+                    </input>
+                    
+                    
+                </div>
+               
+            </div>
+            <br/>
+            <div className="line bottomLine">                
+                <input     
+                    size={1}              
+                    type="text"                         
+                    value={horizontalBottomLeft}        
+                    name="horizontalBottomLeft"
+                    onChange={(e) => {
+                        handleValue(e.target.value, e.target.name)             
+                    }}
+                >
+                </input>
+                <input      
+                    size={1}             
+                    type="text"                         
+                    value={horizontalBottomRight}        
+                    name="horizontalBottomRight"
+                    onChange={(e) => {
+                        handleValue(e.target.value, e.target.name)             
+                    }}
+                >
+                </input>
+            </div>
+            <br/>
+            <div className="footer">
+                <span>
+                    {borderRadiusStyle.borderRadius}
+                </span>
+                <span  
+                    title="Copy CSS to clipboard!"                    
+                    onClick={copyToClipboard} 
+                    className="iconCopy">
+                    <FiCopy />
+                </span>
             </div>
         </div>
     )
--- border-radius-preview/src/index.tsx
+++ border-radius-preview/src/index.tsx
@@ -5,7 +5,11 @@ import App from './App';
 import './index.css'
 
 ReactDOM.render(
-  <React.StrictMode>
+  <React.StrictMode>   
+    <header>
+        <h1>Border Radius Previewer!</h1>
+        <h2>Change some of the 8 values and see the result!</h2>  
+    </header>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
