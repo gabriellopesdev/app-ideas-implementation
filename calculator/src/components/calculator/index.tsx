3667c5c9f78cab6327db417c602b97aeade0e95f
gabriellopesdev <gabslopes34@gmail.com>
2020-08-30 23:04:51 -0300

    Add decimal separator

--- calculator/src/components/calculator/index.tsx
+++ calculator/src/components/calculator/index.tsx
@@ -8,7 +8,7 @@ function Calculator() {
     const [operation, setOperation] = useState('')
     const [editingValue, setEditingValue] = useState('')
 
-    const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    const numbers = ["9", "8", "7", "6", "5", "4", "3",  "2", "1", "0", "."]
 
     function calculate() {
         switch (operation) {
